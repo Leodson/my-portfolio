ad320079517448ba665d2e6cf4817a59014159a6
Leo <leodsonj@gmail.com>
2020-07-15 20:46:52 +0000

    Adds comment section

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -28,7 +28,26 @@ function addRandomGreeting() {
 }
 
 function getMessage() {
-  fetch('/data').then(response => response.text()).then((quote) => {
+  fetch('/data').then(response => response.json()).then((quote) => {
     document.getElementById('message-container').innerHTML = quote;
   });
+}
+
+function generateComments() {
+  fetch('/data').then(response => response.json()).then((comments) => {
+    console.log(comments);
+    const comment_section = document.getElementById('comments');
+    for (var i = 0; i < comments.length; i ++) {
+      comment = createListElement(comments[i]);
+      comment.setAttribute('class', 'comment');
+      comment_section.appendChild(comment);
+    }
+  });
+}
+
+/** Creates an <li> element containing text. */
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
+  return liElement;
 }
